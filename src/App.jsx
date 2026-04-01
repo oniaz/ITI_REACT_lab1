25d2e5e9bc8b48d18459afbfee0968001ab5ba91
oniaz <32391049+oniaz@users.noreply.github.com>
2026-04-02 00:14:57 +0200

    Add Vase 6 product and enhance App component layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -28,22 +28,28 @@ function App() {
       title: "Vase 5",
       price: "$250",
       image: "src/assets/images/5.jpg"
+    },
+    {
+      title: "Vase 6",
+      price: "$350",
+      image: "src/assets/images/6.jpg"
     }
   ];
 
   return (
     <>
-      <h1 className="text-3xl font-bold">ReactJS: Lab 1</h1>
+      <div className="mb-8 text-center">
+        <h1 className="text-3xl font-bold text-gray-800">ReactJS: Lab 1</h1>
+        <h2 className="text-lg text-gray-600 mt-1">Omnia Mohamed</h2>
+      </div>
 
-      {products.map((item, index) => {
-        return <Card key={index}
-          title={item.title}
-          price={item.price}
-          image={item.image}
-        >
-          <Button itemNumber={index + 1} />
-        </Card >;
-      })}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((item, index) => (
+          <Card key={index} title={item.title} price={item.price} image={item.image}>
+            <Button itemNumber={index + 1} />
+          </Card>
+        ))}
+      </div>
     </>
   )
 }
