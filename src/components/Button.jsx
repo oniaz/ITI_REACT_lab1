a4a37c6aa6d512b70f9245bf2acd049db14bd7f1
oniaz <32391049+oniaz@users.noreply.github.com>
2026-04-01 23:52:03 +0200

    Update Button component style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,8 +7,10 @@ export default function Button() {
   return (
     <button
       onClick={handleClickBtn}
-      className="bg-sky-500 text-white px-4 py-2 rounded mt-2 hover:bg-sky-600 transition-colors duration-300"
-    >
+      className="w-full bg-teal-700 text-white py-2 rounded-xl font-medium 
+                 hover:bg-teal-800 active:scale-95 
+                 transition-all duration-200"
+        >
       Click
     </ button>
   )
